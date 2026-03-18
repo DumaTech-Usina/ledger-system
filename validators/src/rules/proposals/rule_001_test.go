c7c3f04723456c6a25a31411ccd5d09fd5c7c5c5
tfla0804 <tfla0804@gmail.com>
2026-03-18 09:15:42 -0300

    Rule001 test implemented.

--- validators/src/rules/proposals/rule_001_test.go
+++ validators/src/rules/proposals/rule_001_test.go
@@ -34,28 +34,28 @@ func (m *MockRows) Scan(dest ...any) error {
 func TestRule001_RunRule001Stream(t *testing.T) {
 	mt := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
 
-	mt.Run("Cenario 1: Nenhuma duplicata encontrada", func(mt *mtest.T) {
-		fakeRows := &MockRows{
+	mt.Run("Scenario 1: No duplicates found", func(mt *mtest.T) {
+		mockRows := &MockRows{
 			Proposals: []Proposal{
-				{ID: "p1", Number: "123", Value: 1000, ClientID: "c1", PlanID: "pl1", EffectiveDate: "2026-01-01"},
-				{ID: "p2", Number: "999", Value: 5000, ClientID: "c2", PlanID: "pl2", EffectiveDate: "2026-02-01"},
+				{ID: "01", Number: "123", Value: 1000, ClientID: "01", PlanID: "01", EffectiveDate: "2026-01-01"},
+				{ID: "02", Number: "999", Value: 5000, ClientID: "02", PlanID: "02", EffectiveDate: "2026-02-01"},
 			},
 		}
 
 		mt.AddMockResponses(mtest.CreateSuccessResponse())
 
-		err := RunRule001Stream(context.Background(), fakeRows, mt.DB)
+		err := RunRule001Stream(context.Background(), mockRows, mt.DB)
 
 		if err != nil {
-			t.Errorf("Não esperava erro, recebeu: %v", err)
+			t.Errorf("Unexpected error: %v", err)
 		}
 	})
 
-	mt.Run("Cenario 2: Duplicata Multidimensional Encontrada", func(mt *mtest.T) {
-		fakeRows := &MockRows{
+	mt.Run("Scenario 2: Multidimensional duplicate found", func(mt *mtest.T) {
+		mockRows := &MockRows{
 			Proposals: []Proposal{
-				{ID: "p1", Number: "123", Value: 1000, ClientID: "c1", PlanID: "pl1", EffectiveDate: "2026-01-01"},
-				{ID: "p2", Number: "0123", Value: 1000, ClientID: "c1", PlanID: "pl2", EffectiveDate: "2026-02-01"},
+				{ID: "01", Number: "123", Value: 1000, ClientID: "01", PlanID: "01", EffectiveDate: "2026-01-01"},
+				{ID: "02", Number: "0123", Value: 1000, ClientID: "01", PlanID: "02", EffectiveDate: "2026-02-01"},
 			},
 		}
 
@@ -65,27 +65,62 @@ func TestRule001_RunRule001Stream(t *testing.T) {
 			mtest.CreateSuccessResponse(),
 		)
 
-		err := RunRule001Stream(context.Background(), fakeRows, mt.DB)
+		err := RunRule001Stream(context.Background(), mockRows, mt.DB)
 
 		if err != nil {
-			t.Errorf("Não esperava erro ao salvar duplicatas, recebeu: %v", err)
+			t.Errorf("Unexpected error when saving duplicates: %v", err)
 		}
 	})
 
-	mt.Run("Cenario 3: Ignorar propostas em branco", func(mt *mtest.T) {
-		fakeRows := &MockRows{
+	mt.Run("Scenario 3: Ignore blank proposals", func(mt *mtest.T) {
+		mockRows := &MockRows{
 			Proposals: []Proposal{
-				{ID: "p1", Number: "   ", Value: 100, ClientID: "c1"},
-				{ID: "p2", Number: "000", Value: 100, ClientID: "c1"},
+				{ID: "01", Number: "   ", Value: 100, ClientID: "01"},
+				{ID: "02", Number: "000", Value: 100, ClientID: "01"},
 			},
 		}
 
 		mt.AddMockResponses(mtest.CreateSuccessResponse())
 
-		err := RunRule001Stream(context.Background(), fakeRows, mt.DB)
+		err := RunRule001Stream(context.Background(), mockRows, mt.DB)
 
 		if err != nil {
-			t.Errorf("Erro ao processar brancos: %v", err)
+			t.Errorf("Error processing blanks: %v", err)
 		}
 	})
+}
+func TestHelpersAndGetters(t *testing.T) {
+	_ = blockingKey("12")
+	_ = blockingKey("123456")
+
+	_ = abs(-10)
+	_ = abs(10)
+
+	_ = min(1, 2, 3)
+	_ = min(3, 1, 2)
+	_ = min(3, 2, 1)
+
+	_ = levenshtein("", "abc")
+	_ = levenshtein("abc", "")
+
+	_ = similar("abc", "a")
+	_ = similar("abc", "abcd")
+	_ = similar("abcd", "abce")
+	_ = similar("abcd", "ab99")
+
+	r2 := Rule002{}
+	_ = r2.RuleID()
+	_ = r2.RuleVersion()
+	_ = r2.BatchSize()
+	_ = r2.Description()
+
+	r3 := Rule003{}
+	_ = r3.RuleID()
+	_ = r3.RuleVersion()
+	_ = r3.Description()
+
+	r4 := Rule004{}
+	_ = r4.RuleID()
+	_ = r4.RuleVersion()
+	_ = r4.Description()
 }
